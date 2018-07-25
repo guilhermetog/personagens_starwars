b15431e94636bf62de0f893d5a069871f417f598
Guilherme Tognetti <email.guilherme.t@gmail.com>
2018-07-25 18:49:26 -0300

    Commit com GHPAGES

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Dashboard from './pages/Dashboard'
 const Roteamento = () => {
     return(
         <Switch>
-            <Route path="/" exact component={Dashboard}/>
+            <Route path="/starwars/" exact component={Dashboard}/>
             <Route path="*" component={()=>(<div>Pagina 404</div>)}/>
         </Switch>
     )
